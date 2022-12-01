e2b366ce979765c7ae2819b012404c19bafb3a61
Rui Oliveira <rpoliveira02@gmail.com>
2022-12-01 10:27:58 +0000

    Add project files.

---
+++ LI4Test/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LI4Test
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
